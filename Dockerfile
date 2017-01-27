b7b08d9aca215b7dab5ed478cf1768e4f3d38c30
Ryan Taylor Long <ryan@rtlong.com>
2017-01-27 00:37:29 -0600

    remove locking stuff, change intended run style

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,16 @@
-FROM alpine
+FROM quay.io/goodguide/base:alpine-3.5
 
-RUN set -x \
- && apk --update add \
+RUN apk --update add \
       bash \
       curl \
       git \
-      jq \
       ncurses \
-      py-pip \
-      zip \
+      zip
 
- && rm -rf /tmp/*
-
-RUN set -x \
- && pip install awscli
-
-RUN set -x \
- && export url='https://releases.hashicorp.com/terraform/0.7.2/terraform_0.7.2_linux_amd64.zip' shasum='c34afce79ee78b3b1a1a5ab7204bba0173c61ce3' \
+RUN version=0.8.5 shasum='4b4324e354c26257f0b830eacb0e7cc7e2ced017d78855f74cb9377f1abf1dd7' \
  && cd /tmp \
- && curl -L -o ./terraform.zip "${url}" \
- && sha1sum ./terraform.zip | grep -q "${shasum}" \
+ && curl -L -o ./terraform.zip "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip" \
+ && sha256sum ./terraform.zip | grep -q "${shasum}" \
  && unzip ./terraform.zip \
  && install ./terraform /usr/local/bin/
 
@@ -31,4 +22,4 @@ ENTRYPOINT ["/bin/_entrypoint"]
 VOLUME /terraform
 WORKDIR /terraform
 
-CMD ["bash"]
+CMD ["help"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,88 +2,13 @@
 
 set -euo pipefail
 # set -x
-
-echo 'welcome to terraform-shell'
-
-[ -f .envrc ] && source .envrc
-
-getUserEmail() {
-	aws iam get-user | jq -r '.User.UserName'
-}
-
-lockResource() {
-	local modulePath="$1"; shift
-	local email="$1"; shift
-	local time="$(date --utc '+%s')"
-
-	local lockObj="$(cat <<-EOF
-		{
-			"module_path": {"S": "${modulePath}"},
-			"user": {"S": "${email}"},
-			"time": {"N": "${time}"}
-		}
-	EOF
-	)"
-
-	local eav="$(cat <<-EOF
-		{
-			":leaseStartTime": {"N": "$(($(date --utc '+%s') - 3600))"}
-		}
-	EOF
-	)"
-
-	aws dynamodb put-item \
-			--table-name terraform-locks \
-			--item "${lockObj}" \
-			--condition-expression "attribute_not_exists(module_path) OR #T < :leaseStartTime" \
-			--expression-attribute-values "${eav}" \
-			--expression-attribute-names '{"#T": "time"}' \
-	|| die "Failed to lock ${modulePath}"
-
-	echo "locked ${modulePath} successfully"
-}
-
-releaseLock() {
-	local modulePath="$1"; shift
-	local email="$1"; shift
-
-	local key="$(cat <<-EOF
-		{
-			"module_path": {"S": "${modulePath}"}
-		}
-	EOF
-	)"
-
-	local eav="$(cat <<-EOF
-		{
-			":user": {"S": "${email}"}
-		}
-	EOF
-	)"
-
-	aws dynamodb delete-item \
-			--table-name terraform-locks \
-			--key "${key}" \
-			--condition-expression "#U = :user" \
-			--expression-attribute-values "${eav}" \
-			--expression-attribute-names '{"#U": "user"}' \
-	|| die "Failed to release lock on ${modulePath}"
-
-	echo "released lock on ${modulePath} successfully"
-}
-
-
 die() {
-	echo "$1"
+	echo "$1" >&2
 	exit 1
 }
 
 main() {
-	me="$(getUserEmail)"
-
-	lockResource "${module_path}" "${me}"
-	bash
-	releaseLock "${module_path}" "${me}"
+	exec terraform "$@"
 }
 
 main $@
